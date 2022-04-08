9d62ff26164c165246aa33281f724efea9cceeeb
Igor Costa <igor@tks.global>
2022-04-08 15:32:11 -0300

    todo list connected whith api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {v4 as uuidv4} from 'uuid'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
@@ -22,6 +23,17 @@ const App = () => {
       completed: true,
     },
   ]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const {data} = await axios.get(
+        'https://jsonplaceholder.cypress.io/todos?_limit=15'
+      )
+      setTasks(data)
+    }
+
+    fetchTasks()
+  }, [])
 
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map(task => {
