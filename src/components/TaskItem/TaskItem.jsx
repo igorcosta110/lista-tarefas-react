ca93eb9964897e04dc990ad853ec1d41431de40c
Igor Costa <igor@tks.global>
2022-04-08 09:15:35 -0300

    icon added for info

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {CgClose} from 'react-icons/cg'
+import { CgClose, CgInfo } from 'react-icons/cg'
 
 import './TaskItem.css'
 
@@ -14,6 +14,9 @@ const TaskItem = ({ task, handleTaskClick, handleTaskDeletion }) => {
             </div>
 
             <div className="buttons-container">
+                <button className="see-task-details-button">
+                    <CgInfo/>
+                </button>
                 <button className="remove-task-button" onClick={()=> handleTaskDeletion(task.id)}>
                     <CgClose/>
                 </button>
